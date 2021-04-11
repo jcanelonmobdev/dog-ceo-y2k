7d2ec028e4404ddb2d570041e222267b1cf60062
Julio César Canelon Rangel <jcanelon@mobdev.cl>
2021-04-11 01:07:19 -0400

    Limpieza de Warnings

--- src/test/java/cl/mobdev/dogceoy2k/application/presentation/BreedDetailControllerImplTest.java
+++ src/test/java/cl/mobdev/dogceoy2k/application/presentation/BreedDetailControllerImplTest.java
@@ -2,9 +2,7 @@ package cl.mobdev.dogceoy2k.application.presentation;
 
 import cl.mobdev.dogceoy2k.application.domain.model.BreedModel;
 import cl.mobdev.dogceoy2k.application.domain.usecase.BreedUseCase;
-import cl.mobdev.dogceoy2k.application.domain.usecase.BreedUseCaseImpl;
 import cl.mobdev.dogceoy2k.application.presentation.body.BreedBody;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,10 +10,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 import java.util.ArrayList;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 class BreedDetailControllerImplTest {
@@ -26,19 +21,23 @@ class BreedDetailControllerImplTest {
 
     private final String BREED_NAME = "bulldog";
 
-    private final String DATA_IMAGES_1 = "https://images.dog.ceo/breeds/bulldog-boston/20200710_175933.jpg";
-    private final String DATA_IMAGES_2 = "https://images.dog.ceo/breeds/bulldog-boston/20200710_175944.jpg";
-    private final String DATA_IMAGES_3 = "https://images.dog.ceo/breeds/bulldog-boston/n02096585_10380.jpg";
 
-    private final String DATA_SUBBREED_1 = "french";
-    private final String DATA_SUBBREED_2 = "english";
-    private final String DATA_SUBBREED_3 = "boston";
+
 
     @Mock
     private BreedUseCase breedUseCaseMock;
 
     @BeforeEach
     void setUp() {
+        // NOTE: Coloque estos atributos como variables para que el IDE no me reclamara (Field can be covered to a local variable)
+        String DATA_IMAGES_1 = "https://images.dog.ceo/breeds/bulldog-boston/20200710_175933.jpg";
+        String DATA_IMAGES_2 = "https://images.dog.ceo/breeds/bulldog-boston/20200710_175944.jpg";
+        String DATA_IMAGES_3 = "https://images.dog.ceo/breeds/bulldog-boston/n02096585_10380.jpg";
+
+        String DATA_SUBBREED_1 = "french";
+        String DATA_SUBBREED_2 = "english";
+        String DATA_SUBBREED_3 = "boston";
+
         MockitoAnnotations.openMocks(this);
         sut = new BreedDetailControllerImpl(breedUseCaseMock);
 
